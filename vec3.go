f1645d1b655e2bd6d3d2d41f25362db5c57c9b28
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-11-08 09:39:03 +0100

    GoDoc for Vec3

--- vec3.go
+++ vec3.go
@@ -13,67 +13,126 @@ func NewVec3xyz(x, y, z float32) Vec3 {
 	return Vec3{x, y, z}
 }
 
-/// Performs a vector addition. (@p v1 + @p v2)
-///  @param[out]	dest	The result vector. [(x, y, z)]
-///  @param[in]		v1		The base vector. [(x, y, z)]
-///  @param[in]		v2		The vector to add to @p v1. [(x, y, z)]
+// dtVadd performs a vector addition. dest = v1 + v2
+//
+//     dest   [out] The result vector.
+//     v1     [in]  The base vector.
+//     v2     [in]  The vector to add to v1.
 func dtVadd(dest, v1, v2 Vec3) {
 	dest[0] = v1[0] + v2[0]
 	dest[1] = v1[1] + v2[1]
 	dest[2] = v1[2] + v2[2]
 }
 
-/// Performs a scaled vector addition. (@p v1 + (@p v2 * @p s))
-///  @param[out]	dest	The result vector. [(x, y, z)]
-///  @param[in]		v1		The base vector. [(x, y, z)]
-///  @param[in]		v2		The vector to scale and add to @p v1. [(x, y, z)]
-///  @param[in]		s		The amount to scale @p v2 by before adding to @p v1.
+// Performs a scaled vector addition. dest = v1 + (v2 * s)
+//
+//     dest   [out] The result vector.
+//     v1     [in]  The base vector.
+//     v1     [in]  The vector to scale and add to v1.
+//     s      [in]  The amount to scale v2 by before adding to v1.
 func dtVmad(dest, v1, v2 Vec3, s float32) {
 	dest[0] = v1[0] + v2[0]*s
 	dest[1] = v1[1] + v2[1]*s
 	dest[2] = v1[2] + v2[2]*s
 }
 
-/// Performs a vector subtraction. (@p v1 - @p v2)
-///  @param[out]	dest	The result vector. [(x, y, z)]
-///  @param[in]		v1		The base vector. [(x, y, z)]
-///  @param[in]		v2		The vector to subtract from @p v1. [(x, y, z)]
+// Performs a vector subtraction. dest = v1 - v2.
+//
+//     dest  [out]  The result vector.
+//     v1    [in]   The base vector.
+//     v2    [in]   The vector to subtract from v1.
 func dtVsub(dest, v1, v2 Vec3) {
 	dest[0] = v1[0] - v2[0]
 	dest[1] = v1[1] - v2[1]
 	dest[2] = v1[2] - v2[2]
 }
 
-/// Performs a vector copy.
-///  @param[out]	dest	The result. [(x, y, z)]
-///  @param[in]		a		The vector to copy. [(x, y, z)]
+// Performs a vector copy. dest = a
+//
+//     dest [out]   The result.
+//     a    [in]    The vector to copy.
 func dtVcopy(dest, a Vec3) {
 	dest[0] = a[0]
 	dest[1] = a[1]
 	dest[2] = a[2]
 }
 
-/// Selects the minimum value of each element from the specified vectors.
-///  @param[in,out]	mn	A vector.  (Will be updated with the result.) [(x, y, z)]
-///  @param[in]	v	A vector. [(x, y, z)]
+// Selects the minimum value of each element from the specified vectors.
+//
+//     mn  [in,out] A vector, will be updated with the result.
+//     v   [in]     A vector.
 func dtVmin(mn, v Vec3) {
 	mn[0] = dtMin(mn[0], v[0])
 	mn[1] = dtMin(mn[1], v[1])
 	mn[2] = dtMin(mn[2], v[2])
 }
 
-/// Selects the maximum value of each element from the specified vectors.
-///  @param[in,out]	mx	A vector.  (Will be updated with the result.) [(x, y, z)]
-///  @param[in]		v	A vector. [(x, y, z)]
+// Selects the maximum value of each element from the specified vectors.
+//
+//     mx  [in,out] A vector, will be updated with the result.
+//     v   [in]     A vector.
 func dtVmax(mx, v Vec3) {
 	mx[0] = dtMax(mx[0], v[0])
 	mx[1] = dtMax(mx[1], v[1])
 	mx[2] = dtMax(mx[2], v[2])
 }
 
+// Derives the square of the scalar length of the vector. (len * len)
+func dtVlenSqr(v Vec3) float32 {
+	return v[0]*v[0] + v[1]*v[1] + v[2]*v[2]
+}
+
+// Returns the distance between two points.
+func dtVdist(v1, v2 Vec3) float32 {
+	dx := v2[0] - v1[0]
+	dy := v2[1] - v1[1]
+	dz := v2[2] - v1[2]
+	return float32(math.Sqrt(float64(dx*dx + dy*dy + dz*dz)))
+}
+
+// Performs a linear interpolation between two vectors. v1 toward v2
+//
+//     dest  [out]  The result vector.
+//     v1    [in]   The starting vector.
+//     v2    [in]   The destination vector.
+//     t     [in]   The interpolation factor. [Limits: 0 <= value <= 1.0]
+func dtVlerp(dest, v1, v2 Vec3, t float32) {
+	dest[0] = v1[0] + (v2[0]-v1[0])*t
+	dest[1] = v1[1] + (v2[1]-v1[1])*t
+	dest[2] = v1[2] + (v2[2]-v1[2])*t
+}
+
+// Derives the cross product of two vectors. dst = v1 x v2
+//
+//     dest   [out] The cross product.
+//     v1     [in]  A Vector.
+//     v2     [in]  A vector.
+func dtVcross(dest, v1, v2 Vec3) {
+	dest[0] = v1[1]*v2[2] - v1[2]*v2[1]
+	dest[1] = v1[2]*v2[0] - v1[0]*v2[2]
+	dest[2] = v1[0]*v2[1] - v1[1]*v2[0]
+}
+
+// Derives the dot product of two vectors. dest = v1 . v2
+//
+//     v1     [in]  A Vector.
+//     v2     [in]  A vector.
+func dtVdot(v1, v2 Vec3) float32 {
+	return v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2]
+}
+
+// Derives the dot product of two vectors on the xz-plane. dest = u . v
+//
+//     u      [in]  A Vector.
+//     v      [in]  A vector.
+// The vectors are projected onto the xz-plane, so the y-values are ignored.
+func dtVdot2D(u, v Vec3) float32 {
+	return u[0]*v[0] + u[2]*v[2]
+}
+
 /// Returns the minimum of two float32 values.
-///  @param[in]		a	Value A
-///  @param[in]		b	Value B
+///  @param[in]        a    Value A
+///  @param[in]        b    Value B
 ///  @return The minimum of the two values.
 func dtMin(a, b float32) float32 {
 	if a < b {
@@ -83,8 +142,8 @@ func dtMin(a, b float32) float32 {
 }
 
 /// Returns the minimum of two int32 values.
-///  @param[in]		a	Value A
-///  @param[in]		b	Value B
+///  @param[in]        a    Value A
+///  @param[in]        b    Value B
 ///  @return The minimum of the two values.
 func dtiMin(a, b int32) int32 {
 	if a < b {
@@ -94,69 +153,12 @@ func dtiMin(a, b int32) int32 {
 }
 
 /// Returns the maximum of two values.
-///  @param[in]		a	Value A
-///  @param[in]		b	Value B
+///  @param[in]        a    Value A
+///  @param[in]        b    Value B
 ///  @return The maximum of the two values.
 func dtMax(a, b float32) float32 {
 	if a > b {
 		return a
 	}
 	return b
-}
-
-/// Derives the square of the scalar length of the vector. (len * len)
-///  @param[in]		v The vector. [(x, y, z)]
-/// @return The square of the scalar length of the vector.
-func dtVlenSqr(v Vec3) float32 {
-	return v[0]*v[0] + v[1]*v[1] + v[2]*v[2]
-}
-
-/// Returns the distance between two points.
-///  @param[in]		v1	A point. [(x, y, z)]
-///  @param[in]		v2	A point. [(x, y, z)]
-/// @return The distance between the two points.
-func dtVdist(v1, v2 Vec3) float32 {
-	dx := v2[0] - v1[0]
-	dy := v2[1] - v1[1]
-	dz := v2[2] - v1[2]
-	return float32(math.Sqrt(float64(dx*dx + dy*dy + dz*dz)))
-}
-
-/// Performs a linear interpolation between two vectors. (@p v1 toward @p v2)
-///  @param[out]	dest	The result vector. [(x, y, x)]
-///  @param[in]		v1		The starting vector.
-///  @param[in]		v2		The destination vector.
-///	 @param[in]		t		The interpolation factor. [Limits: 0 <= value <= 1.0]
-func dtVlerp(dest, v1, v2 Vec3, t float32) {
-	dest[0] = v1[0] + (v2[0]-v1[0])*t
-	dest[1] = v1[1] + (v2[1]-v1[1])*t
-	dest[2] = v1[2] + (v2[2]-v1[2])*t
-}
-
-/// Derives the cross product of two vectors. (@p v1 x @p v2)
-///  @param[out]	dest	The cross product. [(x, y, z)]
-///  @param[in]		v1		A Vector [(x, y, z)]
-///  @param[in]		v2		A vector [(x, y, z)]
-func dtVcross(dest, v1, v2 Vec3) {
-	dest[0] = v1[1]*v2[2] - v1[2]*v2[1]
-	dest[1] = v1[2]*v2[0] - v1[0]*v2[2]
-	dest[2] = v1[0]*v2[1] - v1[1]*v2[0]
-}
-
-/// Derives the dot product of two vectors. (@p v1 . @p v2)
-///  @param[in]		v1	A Vector [(x, y, z)]
-///  @param[in]		v2	A vector [(x, y, z)]
-/// @return The dot product.
-func dtVdot(v1, v2 Vec3) float32 {
-	return v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2]
-}
-
-/// Derives the dot product of two vectors on the xz-plane. (@p u . @p v)
-///  @param[in]		u		A vector [(x, y, z)]
-///  @param[in]		v		A vector [(x, y, z)]
-/// @return The dot product on the xz-plane.
-///
-/// The vectors are projected onto the xz-plane, so the y-values are ignored.
-func dtVdot2D(u, v Vec3) float32 {
-	return u[0]*v[0] + u[2]*v[2]
 }
