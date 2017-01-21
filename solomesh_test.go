1812c1cdd409126004dfa71d440c608497f340e3
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-01-21 01:04:39 +0100

    solomesh_test: done on cube.obj

--- solomesh_test.go
+++ solomesh_test.go
@@ -5,9 +5,9 @@ import "testing"
 func TestCreateNavMesh(t *testing.T) {
 	var meshName string
 
-	//meshName = "testdata/cube.obj"
+	meshName = "testdata/cube.obj"
 	//meshName = "testdata/wallfloors.obj"
-	meshName = "testdata/dungeon.obj"
+	//meshName = "testdata/dungeon.obj"
 	// meshName = "testdata/nav_test.obj"
 	soloMesh := NewSoloMesh()
 	if !soloMesh.Load(meshName) {
