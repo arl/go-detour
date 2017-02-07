3ea63a1f590987d164f40ce9782bed0170c2611b
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-02-07 01:21:42 +0100

    cmd/recast: remove auto-generated code

--- cmd/recast/cmd/root.go
+++ cmd/recast/cmd/root.go
@@ -49,15 +49,6 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports Persistent Flags, which, if defined here,
-	// will be global for your application.
-
-	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.recast.yaml)")
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -66,6 +57,7 @@ func initConfig() {
 		viper.SetConfigFile(cfgFile)
 	}
 
+	// TODO: remove useless statements
 	viper.SetConfigName(".recast") // name of config file (without extension)
 	viper.AddConfigPath("$HOME")   // adding home directory as first search path
 	viper.AutomaticEnv()           // read in environment variables that match
