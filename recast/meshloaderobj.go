d32b42084c7327c6503123b5adc25db8592909b0
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-03-12 14:55:37 +0100

    recast: remove useless fields in MeshLoaderObj

--- recast/meshloaderobj.go
+++ recast/meshloaderobj.go
@@ -8,12 +8,10 @@ import (
 )
 
 type MeshLoaderObj struct {
-	scale     float32
-	verts     []float32
-	tris      []int32
-	normals   []float32
-	vertcount int32
-	tricount  int32
+	scale   float32
+	verts   []float32
+	tris    []int32
+	normals []float32
 }
 
 func NewMeshLoaderObj() *MeshLoaderObj {
@@ -45,7 +43,7 @@ func (mlo *MeshLoaderObj) Load(r io.Reader) error {
 		mlo.verts[i+1] = float32(verts[i/3][1]) * mlo.scale
 		mlo.verts[i+2] = float32(verts[i/3][2]) * mlo.scale
 	}
-	mlo.vertcount = int32(len(verts))
+	vertcount := int32(len(verts))
 
 	// add polygons
 	for _, p := range obj.Polys() {
@@ -53,22 +51,21 @@ func (mlo *MeshLoaderObj) Load(r io.Reader) error {
 			a := p[0]
 			b := p[i-1]
 			c := p[i]
-			if a < 0 || a >= mlo.vertcount || b < 0 || b >= mlo.vertcount || c < 0 || c >= mlo.vertcount {
+			if a < 0 || a >= vertcount || b < 0 || b >= vertcount || c < 0 || c >= vertcount {
 				continue
 			}
 			mlo.tris = append(mlo.tris, a, b, c)
-			mlo.tricount++
 		}
 	}
 
 	// Calculate normals.
 	// TODO: factor this with recast.calcTriNormal
-	mlo.normals = make([]float32, mlo.tricount*3)
-	for i := int32(0); i < mlo.tricount*3; i += 3 {
+	var e0, e1 [3]float32
+	mlo.normals = make([]float32, len(mlo.tris))
+	for i := 0; i < len(mlo.tris); i += 3 {
 		v0 := mlo.verts[mlo.tris[i]*3 : 3+mlo.tris[i]*3]
 		v1 := mlo.verts[mlo.tris[i+1]*3 : 3+mlo.tris[i+1]*3]
 		v2 := mlo.verts[mlo.tris[i+2]*3 : 3+mlo.tris[i+2]*3]
-		var e0, e1 [3]float32
 		for j := 0; j < 3; j++ {
 			e0[j] = v1[j] - v0[j]
 			e1[j] = v2[j] - v0[j]
@@ -106,9 +103,9 @@ func (mlo *MeshLoaderObj) Normals() []float32 {
 }
 
 func (mlo *MeshLoaderObj) VertCount() int32 {
-	return mlo.vertcount
+	return int32(len(mlo.verts) / 3)
 }
 
 func (mlo *MeshLoaderObj) TriCount() int32 {
-	return mlo.tricount
+	return int32(len(mlo.tris) / 3)
 }
