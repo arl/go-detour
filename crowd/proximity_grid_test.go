7916c6c0d0bb650cd268818c3e6238013496812f
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-04-19 21:50:39 +0200

    crowd: add TestProximityGridAddItem

--- crowd/proximity_grid_test.go
+++ crowd/proximity_grid_test.go
@@ -1,7 +1,24 @@
 package crowd
 
-import "testing"
+import (
+	"testing"
 
-func TestProximityGrid(t *testing.T) {
+	"github.com/stretchr/testify/assert"
+)
 
+func TestProximityGridAddItem(t *testing.T) {
+	pg := NewProximityGrid(10, 1)
+	assert.Equal(t, 0, pg.ItemCountAt(1, 1), "grid should be empty")
+
+	pg.AddItem(1, 1, 1, 2, 2)
+	assert.Equal(t, 1, pg.ItemCountAt(1, 1), "should have 1 item in the grid")
+
+	pg.Clear()
+	assert.Equal(t, 0, pg.ItemCountAt(1, 1), "grid should be empty")
+
+	pg.AddItem(1, 1, 1, 2, 2)
+	assert.Equal(t, 1, pg.ItemCountAt(1, 1), "should have 1 item in the grid")
+
+	pg.AddItem(2, 1, 1, 2, 2)
+	assert.Equal(t, 2, pg.ItemCountAt(1, 1), "should have 2 items in the grid")
 }
