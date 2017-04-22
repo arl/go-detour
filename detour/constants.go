ad7f9a73895387485fa3af87251c667db971865b
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-04-22 13:20:52 +0200

    detour: remove type to constants

--- detour/constants.go
+++ detour/constants.go
@@ -1,17 +1,15 @@
 package detour
 
 const (
-	navMeshSetMagic   int32 = 'M'<<24 | 'S'<<16 | 'E'<<8 | 'T' //'MSET';
-	navMeshSetVersion int32 = 1
-)
+	navMeshSetMagic   = 'M'<<24 | 'S'<<16 | 'E'<<8 | 'T' //'MSET';
+	navMeshSetVersion = 1
 
-const (
 	// VertsPerPolygon is the maximum number of vertices per navigation polygon.
-	VertsPerPolygon uint32 = 6
+	VertsPerPolygon = 6
 
 	// A flag that indicates that an off-mesh connection can be traversed in both directions. (Is bidirectional.)
-	offMeshConBidir uint32 = 1
+	offMeshConBidir = 1
 
 	// The maximum number of user defined area ids.
-	maxAreas int32 = 64
+	maxAreas = 64
 )
